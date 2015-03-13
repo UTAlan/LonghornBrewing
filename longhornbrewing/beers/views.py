5abddeaf154f95268720524e86dd4ac97d8aab38
Alan Beam <alan@alanbeam.net>
2015-03-12 20:44:07 -0500

    Fixing Our Beers title

--- longhornbrewing/beers/views.py
+++ longhornbrewing/beers/views.py
@@ -6,7 +6,7 @@ def index(request):
     info = {}
 
     info['ourbeer'] = OurBeer.objects.get(id=1)
-    
+
     info['beers'] = Beer.objects.all()
 
     return render_to_response('beers/index.html', { 'info': info }, context_instance=RequestContext(request))
